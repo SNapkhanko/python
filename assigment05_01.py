10cc2816e0cc294e2cda7ec4326a3260579eca76
SNapkhanko <105534297+SNapkhanko@users.noreply.github.com>
2022-05-23 12:36:26 +0300

    With clear the list before new input

--- assigment05_01.py
+++ assigment05_01.py
@@ -16,20 +16,24 @@ import random
 
 
 l = []
+x = True
 
 
-
-while True:
+while x:
     len_string = int(input("Enter the len of string from 10 till 50"))
-    if 10 > len_string < 50:
+    if 10 < len_string < 50:
+        for x in range(len_string):
+            l.append(random.randint(50, 100))
+        for a in range(len(l)):
+            for b in range(a + 1, len(l)):
+                if l[a] < l[b]:
+                    tmp = l[a]
+                    l[a] = l[b]
+                    lb = tmp
+        print(l)
+        l.clear()
+    else:
         print("You write the wrong len, try again")
         continue
-    for x in range (len_string):
-        l.append(random.randint(50, 100))
-    for a in range(len(l)):
-        for b in range(a+1, len(l)):
-            if l[a] < l[b]:
-                tmp = l[a]
-                l[a]= l[b]
-                lb = tmp
-    print(l)
+
+
